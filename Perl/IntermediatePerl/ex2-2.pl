9f045a8cc781e4d48bcfba011a7794ab5a86bce8
liuxueyang <zypz457@sina.com>
2014-11-09 12:40:40 +0800

    add or modify sth.

--- Perl/IntermediatePerl/ex2-2.pl
+++ Perl/IntermediatePerl/ex2-2.pl
@@ -1,26 +1,24 @@
 #!/usr/bin/env perl 
 #===============================================================================
-#         FILE: ex2-1.pl
+#         FILE: ex2-2.pl
 #       AUTHOR: Phoenix Ikki (liuxueyang.github.io), liuxueyang457@gmail.com
 # ORGANIZATION: Hunan University
-#      CREATED: 11/09/2014 11:36:09
+#      CREATED: 11/09/2014 12:17:32
 #===============================================================================
 
 use strict;
 use warnings;
 use utf8;
 use 5.014;
+use Module::CoreList;
+use ExtUtils::Installed;
 
-use Cwd;
-use File::Spec;
-
-my $path = getcwd;
-opendir(DIR, $path) or die $!;
-while (my $file = readdir(DIR)) {
-	next if ($file =~ m/^\./);
-	my $full_path = Cwd::abs_path($file);
-	say "    $full_path\n";
+my ($inst) = ExtUtils::Installed->new(skip_cwd => 1);
+my (@modules) = $inst->modules();
+for (@modules) {
+	print "$_\t--\t";
+	print Module::CoreList->first_release($_);
+	print "\n";
 }
-closedir(DIR);
 
-exit 0;
+
