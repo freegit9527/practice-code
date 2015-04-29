65ef139040edc3ca6c8a83704bf8ee418d80b92b
liuxueyang <zypz457@sina.com>
2015-04-29 18:59:53 +0800

    add or modify sth.

---
+++ language/ruby/rubylearning/main.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby 
+
+puts 'I am in class = ' + self.class.to_s
+puts 'I am an object = ' + self.to_s 
+print 'The object methods are = '
+puts self.private_methods.sort
+
+
---
+++ language/ruby/rubylearning/swap.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby 
+
+x = 3
+y = 4
+print "x = #{x}, y = #{y}\n"
+
+x, y = y, x
+puts "x = #{x}, y = #{y}"
