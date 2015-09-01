1ccdc88da7c7ff6ca906935ba5dee8f11a7ca35c
liuxueyang <zypz457@sina.com>
2015-09-01 07:15:42 +0000

    add lisp code

---
+++ language/commonlisp/practical_commonlisp/ch6.lisp
@@ -0,0 +1,12 @@
+(defparameter *fn*
+  (let ((count 0))
+    #'(lambda ()
+	(setf count (1+ count)))))
+
+(defparameter *count*
+  (let ((count 0))
+    (list
+     #'(lambda () (incf count))
+     #'(lambda () (decf count))
+     #'(lambda () count))))
+
