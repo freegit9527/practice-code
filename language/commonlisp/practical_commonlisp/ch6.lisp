ab090a51524c712bed2fcb1ae78f6b4b95633c80
liuxueyang <zypz457@sina.com>
2015-09-09 18:15:22 +0800

    add ch7.lisp

--- language/commonlisp/practical_commonlisp/ch6.lisp
+++ language/commonlisp/practical_commonlisp/ch6.lisp
@@ -10,3 +10,22 @@
      #'(lambda () (decf count))
      #'(lambda () count))))
 
+(defvar *x* 10)
+
+(defun foo ()
+  (format t "X: ~d~%" *x*))
+
+(let ((*x* 20))
+  (foo))
+
+(defun bar ()
+  (foo)
+  (let ((*x* 200)) (foo))
+  (foo))
+
+
+(defun foo ()
+  (format t "Before assignment~18tX: ~d~%" *x*)
+  (setf *x* (1+ *x*))
+  (format t "After assignment~18tX: ~d~%~%" *x*))
+
