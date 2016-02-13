6e440a5e0d91d45ffd4a71046224db6ec26968be
liuxueyang <zypz457@sina.com>
2016-02-13 20:03:31 +0800

    map functions in lisp

--- language/commonlisp/onlisp/chapter3_example.lisp
+++ language/commonlisp/onlisp/chapter3_example.lisp
@@ -22,7 +22,7 @@
 
 (= 2 (truncate 2.6)) ; return t. It uses first return value
 
-(format t "~&~S"
+(format t "~%~S"
 	(multiple-value-bind (int frac)
 	    (truncate 2.66)
 	  (list int frac)))
@@ -30,7 +30,7 @@
 (defun powers (x)
   (values x (sqrt x) (expt x 2)))
 
-(format t "~&~S"
+(format t "~%~S"
 	(multiple-value-bind (base root square)
 	    (powers 4)
 	  (list base root square)))
@@ -59,9 +59,9 @@
 
 ; type following three expressions in REPL
 
-(exclaim '(lions and tigers and bears))
+(nconc (exclaim '(lions and tigers and bears)) '(goodness))
 
-(nconc * '(goodness))
+;; (nconc * '(goodness))
 
 (exclaim '(fixnums and bignums and floats))
 
@@ -88,12 +88,15 @@
   a)
 
 ;; use of mapcar, mapcan, maplist, mapc, mapl
-
+;; refer to: https://psg.com/~dlamkins/sl/chapter12.html
+;; mapcar
 (mapcar #'car '((1 a)
                 (2 b)
                 (3 c)))
 (mapcar #'abs '(3 -4 2 -5 -6))
 (mapcar #'cons '(a b c) '(1 2 3))
+
+;; maplist
 (maplist #'append '(1 2 3 4)
          '(1 2)
          '(1 2 3))
@@ -104,19 +107,17 @@
              (if (member (car x) (cdr x))
                  0 1))
          '(a b a c d b c))
-(setq dummy nil)
 
+;; mapc
+(setq dummy nil)
 (mapc #'(lambda (&rest x)
           (setq dummy (append dummy x)))
       '(1 2 3 4)
       '(a b c d e)
       '(x y e))
-(format t "~S" dummy)
-(setq dummy nil)
-(mapl #'(lambda (x)
-          (push x dummy))
-      '(1 2 3 4))
-(format t "~S" dummy)
+(format t " ~S" dummy)
+
+;; mapcan
 (mapcan #'(lambda (x y)
             (if (null x)
                 nil
@@ -134,8 +135,37 @@
                  (list x)))
         '(a 1 b c 3 4 d 5))
 
+;; mapl
+(setq dummy nil)
+(mapl #'(lambda (x) (push x dummy))
+      '(1 2 3 4))
+(format t "dummy: ~S~%" dummy)
+
+;; mapcon
+(mapcon #'list '(1 2 3 4))
+
 ;; use of nconc
 (nconc)
+(setq x '(a b c))
+(setq y '(d e f))
+(nconc x y)
+(format t " x = ~S~%" x)
+
+(setq foo (list 'a 'b 'c 'd 'e)
+      bar (list 'f 'g 'h 'i 'j)
+      baz (list 'k 'l 'm))
+(setq foo (nconc foo bar baz))
+(format t " foo = ~S~%" foo)
+(format t " bar = ~S~%" bar)
+(format t " baz = ~S~%" baz)
+
+(setq foo (list 'a 'b 'c 'd 'e)
+      bar (list 'f 'g 'h 'i 'j)
+      baz (list 'k 'l 'm))
+(setq foo (nconc nil foo bar nil baz))
+(format t " foo = ~S~%" foo)
+(format t " bar = ~S~%" bar)
+(format t " baz = ~S~%" baz)
 
 ;; use of rplaca, rplacd
 (let ((a '(1 2 3)))
@@ -143,13 +173,33 @@
   a)
 (let ((a '(1 2 3)))
   (rplacd a (list 100))
-  (format t "a is: ~S~%" a)
+  (format t "~%a is: ~S~%" a)
   (format t "rplacd return value: ~S~%" (rplacd (last a) (list 200)))
   (format t "~S~%" a)
   (setq lst (list* 'one 'two 'three 'four))
   (format t "~S~%" lst)
   (format t "rplaca return value: ~S~%" (rplaca lst 'uno))
   (format t "~S~%" lst)
-  (rplacd (last lst) (list 'IV)))
+  (format t "rplacd return value: ~S~%" (rplacd (last lst) (list 'IV)))
+  (format t "~S~%" lst))
 
+;; use of map
+(map nil #'+
+     (list 1 2 3)
+     (list 4 5 6))
 
+(map 'list #'+
+     (list 1 2 3)
+     (list 4 5 6))
+
+(map 'vector #'+
+     (list 1 2 3)
+     (list 4 5 6))
+
+(map '(vector number 3) #'+
+     (list 1 2 3)
+     (list 4 5 6))
+
+;; make-sequence
+(make-sequence 'list 0)
+(make-sequence 'string 26 :initial-element #\.)
