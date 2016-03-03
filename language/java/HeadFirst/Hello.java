49e46ca5a686e3ba1e717bdcb4373d5d1cdd15da
liuxueyang <zypz457@sina.com>
2016-03-03 16:43:38 +0800

    add some java code in Head First Java

---
+++ language/java/HeadFirst/Hello.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class Hello {   
+    public static void main(String[] args) {
+        System.out.println("Hello, world~");
+        for (int i = 0; i < 5; ++i) {
+            System.out.print("i = " + i);
+            System.out.println(", ");
+        }
+        /*        
+        Frame f = new Frame();
+        Label l = new Label("Party at Tim's");
+        Button b = new Button("You bet");
+        Button c = new Button("Shoot me");
+        Panel p = new Panel();
+        p.add(l);
+        */
+    }
+}
---
+++ language/java/HeadFirst/MyFirstApp.java
@@ -0,0 +1,14 @@
+public class MyFirstApp {
+    public static void main(String[] args) {
+        System.out.println("I Rule!");
+        System.out.println("The World!");
+
+        int x = 3;
+        String name = "Dirk";
+        x = x * 17;
+        System.out.println("x is " + x);
+
+        double d = Math.random();
+        System.out.println("d is " + d);
+    }
+}
