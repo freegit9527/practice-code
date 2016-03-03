6563c0960b6d7cf5e2685fe98d29a5a4a2c95088
liuxueyang <zypz457@sina.com>
2016-03-03 19:52:58 +0800

    finish chapter 1 in Head First Java

---
+++ language/java/HeadFirst/PoolPuzzleOne.java
@@ -0,0 +1,52 @@
+class PoolPuzzleOne {
+    /*
+      a noise
+      annoys
+      an oyster
+    */
+    public void result() {
+        int x = 0;
+        
+        while (x < 4) {
+            if (x < 1) {
+                System.out.print("a");
+                System.out.print(" ");
+                System.out.print("noise");
+            }
+            x = x - 1;
+            //--
+            if (x == 1) {
+                System.out.print("annoys");
+            }
+            //--
+            if (x > 3) {
+                System.out.print("an");
+                System.out.print(" oyster");
+            }
+            
+            System.out.println("");
+            x = x + 2;
+        }
+    }
+    public static void main(String[] args) {
+        int x = 0;
+
+        while () {
+            //
+            if (x < 1) {
+            }
+
+            if () {
+            }
+
+            if (x == 1) {
+            }
+
+            if () {
+            }
+
+            System.out.println("");
+            //
+        }
+    }
+}
--- language/java/HeadFirst/Shuffle1.java
+++ language/java/HeadFirst/Shuffle1.java
@@ -1,10 +1,15 @@
 class Shuffle1 {
     public static void main(String[] args) {
         int x = 3;
+
+        if (x > 2) {
+            System.out.print("a");
+        }
+
         while (x > 0) {
-            if (x > 2) {
-                System.out.print("a");
-            }
+            x -= 1;
+            System.out.print("-");
+
             if (x == 2) {
                 System.out.print("b c");
             }
@@ -12,8 +17,6 @@ class Shuffle1 {
                 System.out.print("d");
                 x -= 1;
             }
-            x -= 1;
-            System.out.print("-");
         }
     }
 }
