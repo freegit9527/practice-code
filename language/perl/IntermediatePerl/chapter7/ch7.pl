d4127d019fbb49b60b8738488af6e5b4af3efc43
liuxueyang <zypz457@sina.com>
2016-03-06 10:27:08 +0800

    Referencing a Named Subroutine in Perl

--- language/perl/IntermediatePerl/chapter7/ch7.pl
+++ language/perl/IntermediatePerl/chapter7/ch7.pl
@@ -1,37 +1,42 @@
 #!/usr/bin/env perl
 
 use 5.014;
-
-sub hiA {
-    my $person = shift;
-    say "A: $person, I am A.";
-}
-
-sub hiB {
-    my $person = shift;
-    say "B: I guess you are $person, I am B.";
-}
-
-sub hiC {
-    my $person = shift;
-    if ($person eq "A") {
-        say "C: Yes, you are $person. I am C.";
-    } else {
-        say "C: Hello $person, I am C.";
-    }
-    
-}
+use File::Find;
 
 my %people = (
-    A => \&hiA,
-    B => \&hiB,
-    C => \&hiC
-    );
+    A => sub {
+        my $person = shift;
+        say "A: $person, I am A.";
+    },
+    B => sub {
+        my $person = shift;
+        say "B: I guess you are $person, I am B.";
+    },
+    C => sub {
+        my $person = shift;
+        if ($person eq "A") {
+            say "C: Yes, you are $person. I am C.";
+        } else {
+            say "C: Hello $person, I am C.";
+        }
+    });
 
 my @everyone = sort keys %people;
+my @room;
 
 for my $greeter (@everyone) {
-    for my $greeted (@everyone) {
-        $people{$greeter}->($greeted) unless $greeter eq $greeted;
+    say "$greeter gets into the room.";
+    for my $roomate (@room) {
+        $people{$greeter}->($roomate);
+        $people{$roomate}->($greeter);
     }
+    push @room, $greeter;
+    say "";
 }
+
+my @start_directories = qw(.);
+find(
+    sub {
+        say "$File::Find::name";
+    },
+    @start_directories);
