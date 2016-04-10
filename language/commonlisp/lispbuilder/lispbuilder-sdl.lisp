af94b1202adaa2906bce152afa3929b4539f5617
liuxueyang <zypz457@sina.com>
2016-04-10 09:56:23 +0800

    update lisbuilder-sdl example code...

--- language/commonlisp/lispbuilder/lispbuilder-sdl.lisp
+++ language/commonlisp/lispbuilder/lispbuilder-sdl.lisp
@@ -23,7 +23,7 @@
    :icon-caption "liu xue yang")        ; open a window, 320 x 240
   
   (setf (frame-rate) 30)    ; lock the frame rate to 30 fps
-  (sdl:draw-surface (sdl:load-image "~/Desktop/new-face.png"))
+  (sdl:draw-surface (sdl:load-image "./new-face.png"))
   (with-events ()
     (:quit-event () t)      ; absolutely have to handle the :quit-event,
                             ; or the application will not exit.
@@ -39,31 +39,58 @@
            (update-display))))
 
 (defparameter *random-color* sdl:*white*)
+(defparameter *current-shape* 'rectangle)
+(defun set-random-color-shape (shape)
+  (setf
+   *random-color*
+   (sdl:color
+    :r (random 255)
+    :g (random 255)
+    :b (random 255))
+   *current-shape*
+   shape))
+(defun display-shape ()
+  (cond ((sdl:mouse-left-p)
+         (set-random-color-shape 'rectangle))
+        ((sdl:mouse-right-p)
+         (set-random-color-shape 'circle)))
+  (sdl:clear-display sdl:*black*)
+  (cond ((equal *current-shape*
+                'rectangle)
+         (sdl:draw-box
+          (sdl:rectangle-from-midpoint-*
+           (sdl:mouse-x)
+           (sdl:mouse-y)
+           40
+           40)
+          :color *random-color*))
+        ((equal *current-shape* 'circle)
+         (sdl:draw-filled-circle-*
+          (sdl:mouse-x)
+          (sdl:mouse-y)
+          40
+          :color *random-color*))))
 (defun mouse-react-2d ()
-  (sdl:with-init ()
-    (sdl:window 1024 1024 :title-caption "Lpaste")
+  (sdl:with-init
+      ()
+    (sdl:window
+     1024
+     1024
+     :title-caption "Lpaste")
     (setf (sdl:frame-rate) 60)
-    (sdl:with-events ()
+    (sdl:with-events
+        ()
       (:quit-event () t)
       (:key-down-event ()
                        (sdl:push-quit-event))
       (:idle ()
-             ;; change the color of the box if the left mouse
-             ;; button is depressed
-             (when (sdl:mouse-left-p)
-               (setf *random-color* (sdl:color
-                                     :r (random 255)
-                                     :g (random 255)
-                                     :b (random 255))))
-             ;; Clear the display each game loop
-             (sdl:clear-display sdl:*black*)
-             (sdl:draw-box (sdl:rectangle-from-midpoint-*
-                            (sdl:mouse-x) (sdl:mouse-y)
-                            40 40)
-                           :color *random-color*)
-             (sdl:draw-surface-at-* (sdl:load-image "./new-face.png")
-                                    (- (sdl:mouse-x) (/ 256 2))
-                                    (+ 20 (sdl:mouse-y)))
+             (display-shape)
+             (sdl:draw-surface-at-*
+              (sdl:load-image
+               "./new-face.png")
+              (- (sdl:mouse-x) (/ 256 2))
+              (+ 40 (sdl:mouse-y)))
              ;; Redraw the display
              (sdl:update-display)))))
+
 (mouse-react-2d)
