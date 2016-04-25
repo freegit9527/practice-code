f2f453376c6b83f434df19b762af1e1f1d3ceaa5
liuxueyang <zypz457@sina.com>
2016-04-25 17:44:44 +0800

    17.3 and 17.4 of practical common lisp

--- language/common-lisp/practical/ch17.lisp
+++ language/common-lisp/practical/ch17.lisp
@@ -1,16 +1,26 @@
+;; write clean code. ;-)
+
 ;; 17.2, 17.3
 
 (defvar *account-numbers* 0)
 (defclass bank-account ()
   ((customer-name
     :initarg :customer-name
-    :initform (error "Must supply a customer name."))
+    :initform (error "Must supply a customer name.")
+    :accessor customer-name
+    :documentation "Customer's name")
    (balance
     :initarg :balance
-    :initform 0)
+    :initform 0
+    :reader balance
+    :documentation "Current account balance")
    (account-number
-    :initform (incf *account-numbers*))
-   account-type))
+    :initform (incf *account-numbers*)
+    :reader account-number
+    :documentation "Account number, unique within a bank.")
+   (account-type
+    :reader account-type
+    :documentation "Type of account, one of :gold, :silver or :bronze.")))
 
 (defmethod initialize-instance :after ((account bank-account)
                                        &key opening-bonus-percentage)
@@ -43,3 +53,31 @@ Signal an error if the current balance is less than the amount."))
     (error "Account overdrawn."))
   (decf (slot-value account 'balance)
         amount))
+
+;; 17.4
+
+(defgeneric balance (account))
+
+(defmethod balance ((account bank-account))
+  (slot-value account 'balance))
+
+;; 17.5
+
+
+
+(defgeneric (setf customer-name)
+    (value account))
+
+(defmethod (setf customer-name)
+    (value (account bank-account))
+  (setf (slot-value account 'customer-name)
+        value))
+
+(setf (customer-name *account*) "Sally")
+
+(defgeneric customer-name (account))
+
+(defmethod customer-name ((account bank-account))
+  (slot-value account 'customer-name))
+
+(customer-name *account*)
