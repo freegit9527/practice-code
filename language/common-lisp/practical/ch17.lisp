9c1c3d490bba2bac784493dae4630210660da999
liuxueyang <zypz457@sina.com>
2016-04-25 14:27:05 +0800

    update chapter 17 Practical Common Lisp

--- language/common-lisp/practical/ch17.lisp
+++ language/common-lisp/practical/ch17.lisp
@@ -1,16 +1,32 @@
 ;; 17.2
 
+(defvar *account-numbers* 0)
 (defclass bank-account ()
   ((customer-name
     :initarg :customer-name
-    :initform "aries")
+    :initform (error "Must supply a customer name."))
    (balance
     :initarg :balance
-    :initform 0)))
+    :initform 0)
+   (account-number
+    :initform (incf *account-numbers*))
+   account-type))
+
+(defmethod initialize-instance :after ((account bank-account)
+                                       &key opening-bonus-percentage)
+  (let ((balance (slot-value account 'balance)))
+    (setf (slot-value account 'account-type)
+          (cond ((>= balance 100000) :gold)
+                ((>= balance 50000) :silver)
+                (t :bronze)))
+    (when opening-bonus-percentage
+      (incf (slot-value account 'balance)
+            (* balance (/ opening-bonus-percentage 100))))))
 
 (defparameter *account*
   (make-instance 'bank-account
-                 :customer-name "aries-liu"))
-;; (setf (slot-value *account* 'customer-name) "liu")
-;; (setf (slot-value *account* 'balance) 1000)
+                 :customer-name "aries-liu"
+                 :balance 1000
+                 :opening-bonus-percentage 5))
+
 (slot-value *account* 'balance)
