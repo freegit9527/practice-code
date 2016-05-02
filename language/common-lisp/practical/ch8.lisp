58b7a0e082b762741957967351c9079c1b6db44b
liuxueyang <zypz457@sina.com>
2016-05-02 22:48:50 +0800

    update ch8.lisp

--- language/common-lisp/practical/ch8.lisp
+++ language/common-lisp/practical/ch8.lisp
@@ -1,7 +1,17 @@
 (defun primep (number)
-  (loop for i from 2 to (isqrt number)
-     never (zerop (mod number i))))
+  (when (> number 1)
+    (loop for i from 2 to (isqrt number)
+       never (zerop (mod number i)))))
 
 (defun next-prime (number)
   (loop for i from number
      if (primep i) return i))
+
+
+
+(do-primes (i 0 19)
+  (format t "~d" i))
+
+(do ((p (next-prime 0) (next-prime (1+ p))))
+    ((> p 19))
+  (format t "~S " p))
