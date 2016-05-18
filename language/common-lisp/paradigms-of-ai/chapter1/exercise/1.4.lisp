bad3e3ab4c6729ff48045c170361a3885fa2fa59
liuxueyang <zypz457@sina.com>
2016-05-18 17:33:00 +0800

    paradigms exercise in chapter 1

--- language/common-lisp/paradigms-of-ai/chapter1/exercise/1.4.lisp
+++ language/common-lisp/paradigms-of-ai/chapter1/exercise/1.4.lisp
@@ -1,10 +1,12 @@
 (defun count-anywhere (exp lst)
   (if (null lst)
       0
-      (+ (if (equal exp
-                    (first lst))
-             1
-             0)
+      (+ (if (atom (first lst))
+             (if (equal exp
+                        (first lst))
+                 1
+                 0)
+             (count-anywhere exp (first lst)))
          (count-anywhere exp (rest lst)))))
 
 (print (count-anywhere 'a
