f131e69e474a0406bed3914c78691e4570326c7b
liuxueyang <zypz457@sina.com>
2016-07-23 23:17:36 +0800

    WIP GSS4 spoj problem

--- algorithm/spoj/GSS4/main.cpp
+++ algorithm/spoj/GSS4/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 using namespace std;
 
@@ -12,22 +13,24 @@ typedef long long ll;
 typedef struct TreeNode {
   int start, end;
   ll total;
-  ll lazy;
+  bool lazy;
 } TreeNode;
 
 TreeNode tree[4 * 100000 + 10];
 ll a[4 * 100000 + 10];
 
+void update(int stIndex, int start, int end);
+
 void printNode(int stIndex) {
   TreeNode &t = tree[stIndex];
-  printf("(%d, %d), lazy=%lld, total=%lld\n",
-         t.start, t.end, t.lazy, t.total);
+  printf("%d (%d, %d), total=%lld\n",
+         stIndex, t.start, t.end, t.total);
 }
 
 void build(int stIndex, int start, int end) {
   tree[stIndex].start = start;
   tree[stIndex].end = end;
-  tree[stIndex].lazy = 0;
+  tree[stIndex].lazy = false;
 
   if (start == end) {
     tree[stIndex].total = a[start];
@@ -42,25 +45,43 @@ void build(int stIndex, int start, int end) {
     tree[rightChildIndex].total;
 }
 
-void UpdateNode(int stIndex, TreeNode & t, ll value) {
+void UpdateNode(int stIndex, TreeNode & t, int start, int end,
+                bool updating) {
   bool isLeaf = (t.start == t.end);
 
-  t.total += (t.end - t.start + 1) * value;
-  if (!isLeaf) {
-    tree[stIndex * 2].lazy += value;
-    tree[stIndex * 2 + 1].lazy += value;
+  if (isLeaf) {
+    tree[stIndex].total = floor(sqrt(tree[stIndex].total));
   }
-  t.lazy = 0;
+  else {
+    int leftChildIndex = stIndex * 2,
+      rightChildIndex = leftChildIndex + 1;
+
+    if (updating) {
+      tree[leftChildIndex].lazy = tree[rightChildIndex].lazy = true;
+    }
+    else {
+      update(leftChildIndex, start, end);
+      update(rightChildIndex, start, end);
+      tree[stIndex].total = tree[leftChildIndex].total +
+        tree[rightChildIndex].total;
+    }
+  }
 
   return;
+}
+
+bool NecessaryUpdate(int stIndex) {
+  TreeNode & t = tree[stIndex];
+  return t.total > (t.end - t.start + 1);
 }
 
 void update(int stIndex, int start, int end) {
   TreeNode & t = tree[stIndex];
 
-  // needs updated
   if (t.lazy) {
-    UpdateNode(stIndex, t, t.lazy);
+    if (NecessaryUpdate(stIndex)) {
+      UpdateNode(stIndex, t, start, end, true);
+    }
   }
 
   if (start > t.end || end < t.start || start > end) {
@@ -69,14 +90,17 @@ void update(int stIndex, int start, int end) {
 
   // node contain is contained in [start..end]
   if (t.start >= start && t.end <= end) {
-    UpdateNode(stIndex, t);
+    if (NecessaryUpdate(stIndex)) {
+      // cout << stIndex << ", <" << start << ", " << end << ">\n";
+      UpdateNode(stIndex, t, start, end, true);
+    }
     return;
   }
 
   int leftChildIndex = stIndex * 2, rightChildIndex = leftChildIndex + 1;
 
-  update(leftChildIndex, start, end, value);
-  update(rightChildIndex, start, end, value);
+  update(leftChildIndex, start, end);
+  update(rightChildIndex, start, end);
   t.total = tree[leftChildIndex].total + tree[rightChildIndex].total;
 
   return;
@@ -85,9 +109,10 @@ void update(int stIndex, int start, int end) {
 ll query(int stIndex, int start, int end) {
   TreeNode & t = tree[stIndex];
 
-  // needs updated
   if (t.lazy) {
-    UpdateNode(stIndex, t, t.lazy);
+    if (NecessaryUpdate(stIndex)) {
+      UpdateNode(stIndex, t, start, end, false);
+    }
   }
 
   if (start > t.end || end < t.start || start > end) {
@@ -112,7 +137,10 @@ int main(void) {
 
   int N, M;
 
+  int c = 1;
   while (cin >> N) {
+    cout << "Case #" << c << ":" << endl;
+    ++c;
     for (int i = 0; i < N; ++i) {
       cin >> a[i];
     }
@@ -123,17 +151,20 @@ int main(void) {
       int i, x, y;
       cin >> i >> x >> y;
       x--; y--;
-      cout << "Case #" << m << ":" << endl;
       if (i) {
         cout << query(1, x, y) << endl;
       }
       else {
         update(1, x, y);
       }
-      if (m != M) {
-        cout << endl;
-      }
+
+      // print test
+      // for (int ii = 1; ii <= 9; ++ii) {
+      //   printNode(ii);
+      // }
+      // cout << "--------" << endl << endl;
     }
+    cout << endl;
   }
   return 0;
 }
---
+++ algorithm/spoj/GSS4/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(void) {
+  int a = 3;
+  a = floor(sqrt(a));
+  cout << a << endl;
+
+  a = floor(sqrt(a));
+  cout << a << endl;
+  
+  return 0;
+}
