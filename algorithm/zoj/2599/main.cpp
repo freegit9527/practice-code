e13be96c2aa69c4cefdb4936c9a48f92a68e46d8
liuxueyang <zypz457@sina.com>
2016-08-18 22:52:14 +0800

    [WIP] zoj2599

--- algorithm/zoj/2599/main.cpp
+++ algorithm/zoj/2599/main.cpp
@@ -31,31 +31,120 @@ typedef pair<int,int> PII;
 const ll MOD=1000000007;
 ll dp[30][300];
 void initDp(){
-  memset(dp,0,sizeof(dp));
-  rep(i,0,10)dp[1][i]=1;
-  rep(i,2,20){
-    rep(j,0,200){
-      rep(k,0,min(9,j)+1) dp[i][j]+=dp[i-1][j-k];
-    }
-  }
+  dp[0][0]=1;
+  rep(i,1,22)rep(j,0,190)
+    rep(k,0,min(9,j)+1) dp[i][j]+=dp[i-1][j-k];
 }
 // . return nubmer of those whoes number length is len,digit sum is sum
+// O(1)
 ll getSum1(int len,int sum){return dp[len][sum];}
-ll getSum2(int n,int sum){
-  // return numbers in [1..n] whose digit sum is sum
-  ll ans=0;int d=0,a[20];while(n){a[d++]=n%10;n/=10;}
-  rep(j,1,d)rep(i,0,a[d-j])ans+=dp[d-j][sum--];
-  rep(i,0,a[0]+1)if(sum==i)++ans;
+ll getSum2(ll n,int s){
+  // return numbers in [1..n] whose digit-sum is s
+  // (18+18*9)
+  int a[20],b[20],j=0,len,presum=0;
+  ll ans=0,tn=n;
+  while(tn){a[++j]=tn%10;tn/=10;}len=j;
+  rep(i,1,j+1)b[j+1-i]=a[i];
+  rep(k,1,len){
+    rep(i,0,b[k]){
+      if(s>=(presum+i))ans+=getSum1(len-k,s-presum-i);
+    }
+    presum+=b[k];
+  }
+  rep(i,0,b[len]+1)if((presum+i)==s)++ans;
   return ans;
+}
+ll getSum3(ll n,ll prefix,int sum){
+  // 1~n number of numbers whose digit-sum is sum, num-prefix is prefix
+  // O(18)
+  ll ans=0,tn=n,tprefix=prefix;
+  int a[20],b[20],c[20],len_p,len_n,d=0,presum=0;
+  bool mark=false;
+  while(tn){a[++d]=tn%10;tn/=10;}
+  rep(i,1,d+1)b[d+1-i]=a[i];
+  len_n=d;d=0;while(tprefix){a[++d]=tprefix%10;presum+=a[d];tprefix/=10;}
+  len_p=d;rep(i,1,d+1)c[d+1-i]=a[i];
+  rep(i,1,len_p+1){
+    if(c[i]>b[i]){mark=true;break;} //prefix > n_prefix
+    else if(c[i]<b[i]){break;}
+  }
+  if(mark)rep(i,1,len_n-len_p)ans+=getSum1(i,sum-presum);
+  else rep(i,1,len_n-len_p+1)ans+=getSum1(i,sum-presum);
+  if(presum==sum)ans++;
+  return ans;
+}
+ll getSum4(int sum,ll k,ll n){
+  // 1~n whose digit-sum is sum and lexgical<=k
+  // O(18*9)
+  ll ans=0,prefix=0,presum=0;
+  int a[20],b[20],len_k,d=0,tk=k;
+  while(tk){a[++d]=tk%10;tk/=10;}len_k=d;
+  rep(i,1,d+1)b[d+1-i]=a[i];
+  rep(i,1,len_k+1){
+    rep(j,0,b[i]){
+      ll t_prefix=prefix*10+j,t_presum=presum+j;
+      if(t_prefix)ans+=getSum3(n,t_prefix,sum-t_presum);
+    }
+    prefix=prefix*10+b[i];presum+=b[i];
+    if(presum==sum)ans++;
+  }
+  return ans;
+}
+ll getResult2(ll k,ll n){
+  // return number k position
+  ll ans=0;
+  int sum=0,tk=k;
+  while(tk){sum+=tk%10;tk/=10;}
+  rep(i,1,sum){
+    ans+=getSum2(n,i);
+  }
+  cout<<"res1="<<ans
+    <<" res2="<<getSum4(sum,k,n)
+    <<endl;
+  ans=getSum4(sum,k,n);
+  return ans;
+}
+ll getResult1(ll k,ll n){
+  ll prefix=1,cnt;
+  int sum,presum=1;
+  rep(i,1,190){
+    if((cnt=getSum2(n,i))>=k){sum=i;break;}
+    k-=cnt;
+  }
+  while(true){
+    while((cnt=getSum3(n,prefix,sum))<k){
+      k-=cnt;presum++;prefix++;
+    }
+    if(presum==sum)break;
+    prefix*=10;
+  }
+  while(--k)prefix*=10;
+  return prefix;
 }
 
 int main ( void )
 {
 #ifndef  ONLINE_JUDGE
-//  freopen("input.txt", "r", stdin);
-//  freopen("output1.txt","w",stdout);
+  freopen("input1.txt", "r", stdin);
+  freopen("output1.txt","w",stdout);
 #endif     /* -----  ONLINE_JUDGE  ----- */
   initDp();
+  ll n,k;
+    for(int i=25;i<1000;++i){
+      for(int j=9;j<30;++j){
+        int _i=j,_s=0,_t=0;while(_i){_s+=_i%10;_i/=10;_t++;}
+        for(int k=_s;k<=_t*9;++k)cout<<getSum3(i,j,k)<<endl;
+      }
+    }
+  while(cin>>n>>k){
+    if(!n)break;
+//    cout<<"n="<<n
+//      <<" k="<<k
+//      <<endl;
+//    cout<<getResult2(k,n)<<endl;
+//    cout<<getResult1(k,n)<<endl;
+//    cout<<getResult2(k,n)<<" "<<getResult1(k,n)<<endl;
+  }
 
   return EXIT_SUCCESS;
 }				/* ----------  end of function main  ---------- */
---
+++ algorithm/zoj/2599/main1.cpp
@@ -0,0 +1,208 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main1.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  08/18/2016 20:53:09
+ *       Compiler:  g++
+ *
+ *         Author:  Sabastian (liuxueyang.github.io), read3valprintloop@gmail.com
+ *
+ * =====================================================================================
+ */
+ /** 
+    在论文《数位计数问题解法研究——高逸涵》中找到的， 
+    论文中没有代码实现，自己写代码用了很久，又Debug了很久，纠结死了... 
+    题目的第二问，不能用二分， 
+    我之前一直用二分 wrong answer了， 
+    因为1~n不是按照题目规则有序的，所以不能二分。 
+    纠结死我了。。。。。 
+**/  
+#include <iostream>  
+#include <cstdio>  
+#include <cstring>  
+  
+using namespace std;  
+  
+typedef long long LL;  
+  
+LL  dp[20][200];  // dp[L][sum]表示数字位为L 数字和为sum 的数字的个数，(前导零也算)  
+  
+// 数字和为 sum 的 L 位数字的个数（以0为前缀的也算数）  
+LL getSum1(int L, int sum)  
+{  
+    if ( sum > 9*L || L < 0 || sum < 0 )  
+        return 0;  
+    if ( dp[L][sum] )  
+        return dp[L][sum];  
+    if ( L == 0 && sum == 0 )  
+        return 1;  
+    for (int i = 0; i <= 9; i++)  
+    {  
+        if ( sum-i < 0 ) break;  
+        dp[L][sum] += getSum1(L-1, sum-i);  
+    }  
+    return dp[L][sum];  
+}  
+  
+// 返回 1~n 中数字和为 sum 的数的个数  
+LL getSum2(LL n, int sum)  
+{  
+    int digit[20], L = 0;  
+  
+    while ( n )  
+    {  
+        digit[L++] = n%10;  
+        n /= 10;  
+    }  
+  
+    LL  res = 0LL;  
+    for (int i = L-1; i >= 0; i--)  
+    {  
+        for (int j = 0; j < digit[i]; j++)  
+            res += getSum1(i, sum--);  
+    }  
+    res += getSum1(0, sum);  
+    return res;  
+}  
+  
+// 返回 1~n 中数字和为 sum 前缀为 prefix 的数的个数  
+LL getSum3(LL n, LL prefix, int sum)  
+{  
+    char sn[21] = {0}, sp[21] = {0};  
+    int  ln, lp;  
+  
+    sprintf(sn, "%lld", n);  
+    sprintf(sp, "%lld", prefix);  
+    ln = strlen(sn);  
+    lp = strlen(sp);  
+  
+    for (int i = 0; i < lp; i++)  
+        sum -= sp[i] - '0';  
+  
+    int i;  
+    for (i = 0; i < lp; i++)  
+    {  
+        if ( sn[i] != sp[i] )  
+            break;  
+    }  
+    if ( i < lp )  
+    {  
+        LL  res = 0LL;  
+        if ( sn[i] < sp[i] )  ln--;  
+        for (i = ln-lp; i >= 0; i--)  
+            res += getSum1(i, sum);  
+        return res;  
+    }  
+  
+    LL  t = 0LL, res = 0LL;  
+    for (i = lp; i < ln; i++)  
+        t = 10*t + sn[i] - '0';  
+    res = getSum2(t, sum);  
+    for (i = ln-lp-1; i >= 0; i--)  
+        res += getSum1(i, sum);  
+    return res;  
+}  
+  
+// 返回 1~n 中数字和为 sum 且字典序小于k的数的个数  
+LL getSum4(LL n, LL k, int sum)  
+{  
+    int digit[20], L = 0;  
+  
+    while ( k )  
+    {  
+        digit[L++] = k % 10;  
+        k /= 10;  
+    }  
+  
+    LL  prefix = 1LL, res = 0LL;  
+    int t = 1;  
+    for (int i = L-1; i >= 0; i--)  
+    {  
+        for (int j = t; j < digit[i]; j++)  
+        {  
+            res += getSum3(n, prefix++, sum);  
+        }  
+        prefix = 10*prefix;  
+        t = 0;  
+    }  
+  
+    // 如果 k=3000; 小于k的数有3,30,300;而上面的计算不包括这些  
+    // 所以下面特殊车里这种数据  
+    for (int i = 0; i < L; i++)  
+    {  
+        if (digit[i] == 0) res++;  
+        else break;  
+    }  
+    return res;  
+}  
+  
+// 返回 k 在 1~n 中的位置  
+LL getSum5(LL n, LL k)  
+{  
+    int sum = 0;  
+    LL  _k = k;  
+  
+    while ( _k )  
+    {  
+        sum += _k % 10;  
+        _k /= 10;  
+    }  
+  
+    LL  res = 0LL;  
+    for (int i = 1; i < sum; i++) // 数字和小于sum的数的个数  
+        res += getSum2(n, i);  
+    cout<<"res1="<<res
+      <<" res2="<<getSum4(n,k,sum)
+      <<endl;
+    res += getSum4(n, k, sum);   // 数字和为sum且字典序小于k的数的个数  
+    return res + 1;  
+}  
+  
+int main()  
+{  
+#ifndef  ONLINE_JUDGE
+  freopen("input1.txt", "r", stdin);
+  freopen("output2.txt","w",stdout);
+#endif     /* -----  ONLINE_JUDGE  ----- */
+    LL  n, k;  
+  
+    for(int i=25;i<1000;++i){
+      for(int j=9;j<30;++j){
+        int _i=j,_s=0,_t=0;while(_i){_s+=_i%10;_i/=10;_t++;}
+        for(int k=_s;k<=_t*9;++k)cout<<getSum3(i,j,k)<<endl;
+      }
+    }
+    while ( cin >> n >> k && n )  
+    {  
+//        cout << getSum5(n, k) << " ";  
+  
+        int sum = 1, preSum;  
+        LL  t, pre;  
+        while ( (t = getSum2(n, sum)) < k )  
+        {  
+            sum++;  
+            k -= t;  
+        }  
+        pre = 1;  
+        preSum = 1;  
+
+        while ( true )  
+        {  
+            while ( (t = getSum3(n, pre, sum)) < k )  
+            {  
+                pre++;  
+                preSum++;  
+                k -= t;  
+            }  
+            if ( preSum == sum ) break;  
+            else pre *= 10;  
+        }  
+        while ( --k ) pre *= 10;  
+//        cout << pre << endl;  
+    }  
+    return 0;  
+}  
